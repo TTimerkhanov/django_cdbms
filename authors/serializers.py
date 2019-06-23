6f99d0d835310398677f32a0eb9c2aa645bbcfd0
timurtimerhanov <timurgrunge@gmail.com>
2019-06-23 12:22:07 +0300

    Initial

--- authors/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from authors.models import Author
-
-
-class AuthorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        exclude = ()
--- books/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from books.views import HelloView
-
-urlpatterns = [
-    path('hello', HelloView.as_view(), name='hello')
-]
